84908ebabc2ddfe5af5a0c3c3bc947f0927f6a1c
personnameds <personnameds@gmail.com>
2020-04-19 22:21:38 -0400

    Player URL
    Displays Player URL
    Small tweaks for server

--- clue/settings.py
+++ clue/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_DIRECTORY=os.path.join(BASE_DIR,'fitbiting')+'/SECRET_KEY'
+SECRET_DIRECTORY=os.path.join(BASE_DIR,'clue')+'/SECRET_KEY'
 with open(SECRET_DIRECTORY) as f:
     SECRET_KEY = f.read().strip()
 
